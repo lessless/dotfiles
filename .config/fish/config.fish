a37121197dd344f72426636ec5956facb63f3ee0
lessless <yevhenii.kurtov@gmail.com>
2025-09-26 18:36:37 +0100

    Theming and graphics

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go herei
     fzf --fish | source
+    alias dfiles='/usr/local/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+    source (/usr/local/bin/starship init fish --print-full-init | psub)
 end
-/usr/local/bin/mise activate  fish | source
+/usr/local/bin/mise activate fish | source
